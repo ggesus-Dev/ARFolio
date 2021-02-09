c3b964aa2ccd8c4f3d3615a6b5df6cd7bb337361
Hayden D.R <hd.rydell@outlook.com>
2021-02-09 17:11:36 +1100

    Fixed CreateFolio script errors, updated methods that relied on the UnityEngine package

--- ARFolioBundler/Assets/Editor/CreateFolio.cs
+++ ARFolioBundler/Assets/Editor/CreateFolio.cs
@@ -6,12 +6,14 @@ using UnityEditor;
 public class PrepareBatch : AssetPostprocessor {
     // Before any asset is imported
     static void OnPreprocessAsset() {
-        if(!Application.isBatchMode) return;
+        // if(!Application.isBatchMode) return;
+        if(!UnityEditorInternal.InternalEditorUtility.inBatchMode) return;
 
         bool _debug = false;
         string _folioPath = "Assets/Test";
         string _outputPath = "Assets/LocalDB";
-        string _filePath = "";
+        string _file = "";
+        string _name = "ggesus-folio";
         string[] _files = Directory.GetFiles(_folioPath);
         // user information
 
@@ -19,34 +21,35 @@ public class PrepareBatch : AssetPostprocessor {
             for(int i = _files.Length - 1; i > 0; i--) {
                 if(_debug) debugReport(0, _files[i]);
 
-                if(_files[i]).EndsWith(".png") || _files[i].EndsWith(".jpg") || _files[i].EndsWith(".fbx")) {
+                if((_files[i]).EndsWith(".png") || _files[i].EndsWith(".jpg") || _files[i].EndsWith(".fbx")) {
                     _file = _files[i].Replace(@"/", @"\");
-                    AssetImporter.GetAtPath(_file).SetAssetBundleNameAndVariant("ggesus-folio", "fd");
+                    AssetImporter.GetAtPath(_file).SetAssetBundleNameAndVariant(_name, "fd");
                     
-                    if(_debug) debugReport(1, _file);
+                    if(_debug) debugReport(1, _file, _name);
                 }
             }
         }
 
-        if(!System.IO.Directory.Exists(_outputPath)) 
-            System.IO.Directory.CreateDirectory(_outputPath);
+        if(!Directory.Exists(_outputPath)) 
+            Directory.CreateDirectory(_outputPath);
         
-        BuildPipeline.BuildAssetBundles(_outputPath, BuildAssetBundlesOptions.None, BuildTarget.Android);
+        BuildPipeline.BuildAssetBundles(_outputPath, BuildAssetBundleOptions.None, BuildTarget.Android);
 
-        if(_debug) 
+        if(_debug) {
             string _logPath = "C:/Users/hayde/AppData/Local/Unity/Editor/Editor.log";
-			Application.OpenURL(_logPath);
+            System.Diagnostics.Process.Start(_logPath);
+        }
     }
 
-    static void debugReport(int type, var args[]) {
+    static void debugReport(int type, params string[] args) {
         string _message = "";
         
         switch(type) {
             case 0:     _message = $"LILIA: File being looked at is: {args[0]}";                                break;
-            case 1:     _message = $"LILIA: File name {args[0]} was updated and added to ggesus-folio.fd";      break;
+            case 1:     _message = $"LILIA: File name {args[0]} was updated and added to {args[1]}.fd";         break;
             default:    _message = "LILIA: Unknown report.";                                                    break; 
         }
 
-        Debug.Log(_message);
+    System.Console.WriteLine(_message);
     }
 }
---
+++ ARFolioBundler/RunCreateFolio.bat
@@ -0,0 +1,22 @@
+@ECHO ON
+
+GOTO IS_RUNNING
+
+:IS_RUNNING
+ECHO Checking if Unity is running...
+TASKLIST /fi "imagename eq Unity.exe" 2>NUL | FIND /i /n "unity.*">NUL
+IF ERRORLEVEL 1 (
+    ECHO Trying to run Unity batchmode
+    GOTO RUN_UNITY_BATCHMODE 
+) ELSE (
+    ECHO Found Unity process, removing it now...
+    TASKKILL /fi /f "imagename eq Unity.exe"
+)
+
+:RUN_UNITY_BATCHMODE
+ECHO Attempting to open Unity in Batchmode...
+PAUSE
+SET UnityPath="D:/Programs/Unity/2019.4.19f1/Editor/Unity.exe"
+SET StartupPath="D:\GIT\ARFolio\ARFolioBundler"
+PAUSE
+%UnityPath% -batchmode -quit -projectPath %StartupPath% -executeMethod CreateFolio.OnPreprocessAsset
