ecc2c6dc5cd8f3ac814fbcede725a07b1176b3c6
Hayden D.R <hd.rydell@outlook.com>
2021-02-09 17:41:19 +1100

    Fixed TASKILL command; just searching and terminating for unity.exe now

--- ARFolioBundler/RunCreateFolio.bat
+++ ARFolioBundler/RunCreateFolio.bat
@@ -4,13 +4,13 @@ GOTO IS_RUNNING
 
 :IS_RUNNING
 ECHO Checking if Unity is running...
-TASKLIST /fi "imagename eq Unity.exe" 2>NUL | FIND /i /n "unity.*">NUL
+TASKLIST /fi "imagename eq unity.exe" 2>NUL | FIND /i /n "unity.exe">NUL
 IF ERRORLEVEL 1 (
     ECHO Trying to run Unity batchmode
     GOTO RUN_UNITY_BATCHMODE 
 ) ELSE (
     ECHO Found Unity process, removing it now...
-    TASKKILL /fi /f "imagename eq Unity.exe"
+    TASKKILL /f /fi "imagename eq unity.exe"
 )
 
 :RUN_UNITY_BATCHMODE
@@ -18,3 +18,5 @@ ECHO Attempting to open Unity in Batchmode...
 SET UnityPath="D:\Programs\Unity\2019.4.19f1\Editor\Unity.exe"
 SET StartupPath="D:\GIT\ARFolio\ARFolioBundler"
 %UnityPath% -batchmode -quit -projectPath %StartupPath% -executeMethod CreateFolio.OnPreprocessAsset
+
+PAUSE
