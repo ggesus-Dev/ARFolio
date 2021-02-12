53c0954ed9d31a0395e40a9c0712c7709cee349a
Hayden D.R <hd.rydell@outlook.com>
2021-02-12 12:06:06 +1100

    Added second list for path references; moved blocks of code around

--- ARFolioMain/Assets/Scripts/ListFolios.cs
+++ ARFolioMain/Assets/Scripts/ListFolios.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class ListFolios : MonoBehaviour {
     public  GameObject          activeFolio;
-    private List<GameObject>    _folioList = new List<GameObject>();
+    private List<string>        _folioList = new List<string>();
+    private List<GameObject>    _optBtnList = new List<GameObject>();
 
     void Awake() {
         // What platform are we on
@@ -23,25 +24,33 @@ public class ListFolios : MonoBehaviour {
             return;                                 // dont need to continue
         }
         
+        checkFolios(_path);
+        drawButtons();
+    }
+
+    // Scan for available folios and add them to list
+    void checkFolios(string path) {
+        string[] _files = Directory.GetFiles(path);
+        _folioList.Clear();
+
+        for(int i = 0; i < _files.Length; i++) {
+            if(_files[i].EndsWith(".fd")) 
+                _folioList.Add(_files[i]);
+        }
+    }
+
+    // Draw buttons for each found folio
+    void drawButtons() {
         GameObject  _btnPrefab = (GameObject)Resources.Load("Prefabs/BtnFolioOption");
         Transform   _parent = gameObject.transform.GetChild(0).GetChild(0);
         float       _xbuffer = 20f, 
                     _ybuffer = 20f,
                     _btnWidth = _btnPrefab.GetComponent<RectTransform>().rect.width, 
                     _btnHeight = _btnPrefab.GetComponent<RectTransform>().rect.height;
-        string[]    _files = Directory.GetFiles(_path);
 
-        for(int i = 0; i < _files.Length; i++) {
-            if(_files[i].EndsWith(".fd")) {
-                Debug.Log(_files[i]);
-                Vector3 _pos = new Vector3((_btnWidth * i) + (_xbuffer * (i + 1)), 100f + _ybuffer, 0);
-                _folioList.Add(Instantiate(_btnPrefab, _pos, Quaternion.identity, _parent));
-            }
+        for(int i = 0; i < _folioList.Count; i++) {
+            Vector3 _pos = new Vector3((_btnWidth * i) + (_xbuffer * (i + 1)), 100f + _ybuffer, 0);
+            _optBtnList.Add(Instantiate(_btnPrefab, _pos, Quaternion.identity, _parent));
         }
-
-    }
-
-    void Update() {
-        
     }
 }
