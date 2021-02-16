41339dabc0652003bb81864edc4209e7114898a7
Hayden D.R <hd.rydell@outlook.com>
2021-02-16 13:54:32 +1100

    Changed SelectionOpen script to a general Canvas Controller

---
+++ ARFolioMain/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour {
+    public Vector2 activeBetweenStartPoint, activeBetweenEndPoint;
+
+    private void Awake() {
+        SwipeController.onSwipe += swipeControllerOnSwipe;
+    }
+
+    private void swipeControllerOnSwipe(SwipeController.SwipeData data) {
+        switch(data.direction) {
+            case SwipeController.SwipeDirection.UP: 
+                if(data.startPosition.y < activeBetweenEndPoint.y) {
+                    GameObject _selectionView = gameObject.transform.GetChild(0).gameObject;
+                    _selectionView.SetActive(true);
+                }
+                break;
+            case SwipeController.SwipeDirection.DOWN: 
+                break;
+            case SwipeController.SwipeDirection.LEFT: 
+                break;
+            case SwipeController.SwipeDirection.RIGHT: 
+                break;
+        }
+
+        
+        if(data.direction == SwipeController.SwipeDirection.UP) {
+            
+        }
+    }
+
+    private void onComplete() {
+        Destroy(gameObject);
+        
+    }
+}
