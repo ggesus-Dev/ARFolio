a8abe9d26d6e0a613d3507702fe1cdef40d6f9fc
Hayden D.R <hd.rydell@outlook.com>
2021-02-16 16:04:33 +1100

    Opening and closing of the selection menu and ability to extend it to others later

--- ARFolioMain/Assets/Scripts/CanvasController.cs
+++ ARFolioMain/Assets/Scripts/CanvasController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CanvasController : MonoBehaviour {
     public Vector2 activeBetweenStartPoint, activeBetweenEndPoint;
+    private bool _selectionOpen = false;
+    private GameObject _mySelectionView;
 
     private void Awake() {
         SwipeController.onSwipe += swipeControllerOnSwipe;
@@ -13,26 +15,34 @@ public class CanvasController : MonoBehaviour {
         switch(data.direction) {
             case SwipeController.SwipeDirection.UP: 
                 if(data.startPosition.y < activeBetweenEndPoint.y) {
-                    GameObject _selectionView = gameObject.transform.GetChild(0).gameObject;
-                    _selectionView.SetActive(true);
+                    selectionController();
                 }
                 break;
             case SwipeController.SwipeDirection.DOWN: 
+                if(data.startPosition.y > activeBetweenEndPoint.y) {
+                    selectionController();
+                }
                 break;
             case SwipeController.SwipeDirection.LEFT: 
                 break;
             case SwipeController.SwipeDirection.RIGHT: 
                 break;
         }
-
-        
-        if(data.direction == SwipeController.SwipeDirection.UP) {
-            
-        }
     }
 
-    private void onComplete() {
-        Destroy(gameObject);
-        
+    private static void onComplete(object toDestroy) {
+        Destroy((GameObject)toDestroy);
+    }
+
+    private void selectionController() { 
+        if(!_selectionOpen) {
+            GameObject _viewportSelection = (GameObject)Resources.Load("Prefabs/ViewportSelection");                        // get prefab reference
+            _mySelectionView = Instantiate(_viewportSelection, gameObject.transform, false);                                // create and make its parent the canvas
+            LeanTween.moveY(gameObject, 150f, 0.1f);                                                                        // tween animation
+            _selectionOpen = true;
+        } else {                                                                                                            // if already open
+            LeanTween.moveY(_mySelectionView, 0f, 0.1f).setOnComplete(onComplete, _mySelectionView);
+            _selectionOpen = false;
+        }
     }
 }
