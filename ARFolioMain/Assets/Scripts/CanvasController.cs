cbe2368fddd26549323300bbc98f83dfb5508686
Hayden D.R <hd.rydell@outlook.com>
2021-02-18 21:08:43 +1100

    Removed open boolean, replaced by general parameter in function; added debug text visable in UnityPlayer; input added if we're in debugging in editor

--- ARFolioMain/Assets/Scripts/CanvasController.cs
+++ ARFolioMain/Assets/Scripts/CanvasController.cs
@@ -4,34 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour {
-    public Vector2 activeBetweenStartPoint, activeBetweenEndPoint;
-    private bool _selectionOpen = false;
-    private GameObject _mySelectionView;
+    public Vector2 screenRes;
+    private GameObject _mySelectionView = null;
     private GameObject _debugText;
     public  LeanTweenType _easeType;
 
     private void Awake() {
+        screenRes = new Vector2(Screen.width, Screen.height);
         _debugText = transform.GetChild(0).gameObject;
         SwipeController.onSwipe += swipeControllerOnSwipe;
     }
 
+    # if UNITY_EDITOR
     private void Update() {
         if(Input.GetMouseButtonDown(0)) {
-            selectionController();
+            if(_mySelectionView == null) selectionController(true); else selectionController(false);
         }
     }
+    # endif
 
     private void swipeControllerOnSwipe(SwipeController.SwipeData data) {
         switch(data.direction) {
             case SwipeController.SwipeDirection.UP: 
-                if(data.startPosition.y < activeBetweenEndPoint.y) {
-                    selectionController();
-                    _debugText.GetComponent<Text>().text += "\nUP";
+                // start to swipe up within the lower third of the screen 
+                if(data.startPosition.y < screenRes.y / 3) {
+                    selectionController(true);
+                    _debugText.GetComponent<Text>().text += "\nSWIPE UP";
                 }
                 break;
             case SwipeController.SwipeDirection.DOWN: 
-                if(data.startPosition.y > activeBetweenEndPoint.y) {
-                    selectionController();
+                // start to swipe above 
+                if(data.startPosition.y > screenRes.y / 3) {
+                    selectionController(false);
                     _debugText.GetComponent<Text>().text += "\nDOWN";
                 }
                 break;
@@ -46,15 +50,14 @@ public class CanvasController : MonoBehaviour {
         Destroy((GameObject)toDestroy);
     }
 
-    private void selectionController() { 
-        if(!_selectionOpen) {
+    private void selectionController(bool open) { 
+        if(open && _mySelectionView == null) {
             GameObject _viewportSelection = (GameObject)Resources.Load("Prefabs/ViewportSelection");                        // get prefab reference
             _mySelectionView = Instantiate(_viewportSelection, gameObject.transform, false);                                // create and make its parent the canvas
             LeanTween.moveY(_mySelectionView, 150f, 0.2f).setEase(_easeType);                                               // tween animation
-            _selectionOpen = true;
-        } else {                                                                                                            // if already open
-            LeanTween.moveY(_mySelectionView, -200f, 0.1f).setOnComplete(onComplete, _mySelectionView).setEase(_easeType);
-            _selectionOpen = false;
+        } else if(!open && _mySelectionView != null) {                                                                                                            // if already open
+            LeanTween.moveY(_mySelectionView, -200f, 0.1f).setEase(_easeType).setOnComplete(onComplete, _mySelectionView);
+            _mySelectionView = null;
         }
     }
 }
