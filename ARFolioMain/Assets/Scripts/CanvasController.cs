1c634c11655ef206164785ce8ba6f19be38d2d7b
Hayden D.R <hd.rydell@outlook.com>
2021-02-18 17:08:55 +1100

    Added debug text to screen, fixed rapid movement of selection panel bug, added an easetype to selection panel

--- ARFolioMain/Assets/Scripts/CanvasController.cs
+++ ARFolioMain/Assets/Scripts/CanvasController.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour {
     public Vector2 activeBetweenStartPoint, activeBetweenEndPoint;
     private bool _selectionOpen = false;
     private GameObject _mySelectionView;
+    private GameObject _debugText;
+    public  LeanTweenType _easeType;
 
     private void Awake() {
+        _debugText = transform.GetChild(0).gameObject;
         SwipeController.onSwipe += swipeControllerOnSwipe;
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            selectionController();
+        }
     }
 
     private void swipeControllerOnSwipe(SwipeController.SwipeData data) {
@@ -16,11 +26,13 @@ public class CanvasController : MonoBehaviour {
             case SwipeController.SwipeDirection.UP: 
                 if(data.startPosition.y < activeBetweenEndPoint.y) {
                     selectionController();
+                    _debugText.GetComponent<Text>().text += "\nUP";
                 }
                 break;
             case SwipeController.SwipeDirection.DOWN: 
                 if(data.startPosition.y > activeBetweenEndPoint.y) {
                     selectionController();
+                    _debugText.GetComponent<Text>().text += "\nDOWN";
                 }
                 break;
             case SwipeController.SwipeDirection.LEFT: 
@@ -38,10 +50,10 @@ public class CanvasController : MonoBehaviour {
         if(!_selectionOpen) {
             GameObject _viewportSelection = (GameObject)Resources.Load("Prefabs/ViewportSelection");                        // get prefab reference
             _mySelectionView = Instantiate(_viewportSelection, gameObject.transform, false);                                // create and make its parent the canvas
-            LeanTween.moveY(gameObject, 150f, 0.1f);                                                                        // tween animation
+            LeanTween.moveY(_mySelectionView, 150f, 0.2f).setEase(_easeType);                                               // tween animation
             _selectionOpen = true;
         } else {                                                                                                            // if already open
-            LeanTween.moveY(_mySelectionView, 0f, 0.1f).setOnComplete(onComplete, _mySelectionView);
+            LeanTween.moveY(_mySelectionView, -200f, 0.1f).setOnComplete(onComplete, _mySelectionView).setEase(_easeType);
             _selectionOpen = false;
         }
     }
--- ARFolioMain/Assets/Scripts/SwipeController.cs
+++ ARFolioMain/Assets/Scripts/SwipeController.cs
@@ -9,7 +9,7 @@ public class SwipeController : MonoBehaviour {
     [SerializeField]
     private Vector2 _startPos, _endPos;
 
-    public bool _detectOnRelease = true; 
+    private bool _detectOnRelease = true; 
 
     // thanks to Jason Weimann and whoever he took parts from for SendSwipe function
     public struct SwipeData {
@@ -41,29 +41,8 @@ public class SwipeController : MonoBehaviour {
                     break;
                 case TouchPhase.Ended:
                     _endPos = _touch.position;
+                    getSwipeData();
                     break;
-            }
-
-            float _horDistance = Mathf.Abs(_startPos.x - _endPos.y);
-            float _verDistance = Mathf.Abs(_startPos.y - _endPos.y);
-            if(checkSwipe(_horDistance, _verDistance)) {
-                SwipeDirection _dir;                    // direction user swiped in
-            
-                if(_verDistance > _horDistance) {       // if it was a vertical swipe
-                    Debug.Log("Vertical Swipe");
-                    
-                    if(_endPos.y > _startPos.y) 
-                        _dir = SwipeDirection.UP; else _dir = SwipeDirection.DOWN;      // if our end position is greater than our start, direction is up
-                } else {
-                    Debug.Log("Horizontal Swipe");
-
-                    if(_endPos.x > _startPos.x)
-                        _dir = SwipeDirection.LEFT; else _dir = SwipeDirection.RIGHT;
-                }
-
-                sendSwipe(_dir);
-            } else {
-                Debug.Log("Tapped screen");
             }
         }
     }
@@ -71,6 +50,30 @@ public class SwipeController : MonoBehaviour {
     private bool checkSwipe(float horDistance, float verDistance) {
         if(verDistance > _minDistance || horDistance > _minDistance) return true;       // swiped
         return false;                                                                   // treat as tap
+    }
+
+    private void getSwipeData() {
+        float _horDistance = Mathf.Abs(_startPos.x - _endPos.y);
+        float _verDistance = Mathf.Abs(_startPos.y - _endPos.y);
+        if(checkSwipe(_horDistance, _verDistance)) {
+            SwipeDirection _dir;                    // direction user swiped in
+        
+            if(_verDistance > _horDistance) {       // if it was a vertical swipe
+                Debug.Log("Vertical Swipe");
+                
+                if(_endPos.y > _startPos.y) 
+                    _dir = SwipeDirection.UP; else _dir = SwipeDirection.DOWN;      // if our end position is greater than our start, direction is up
+            } else {
+                Debug.Log("Horizontal Swipe");
+
+                if(_endPos.x > _startPos.x)
+                    _dir = SwipeDirection.LEFT; else _dir = SwipeDirection.RIGHT;
+            }
+
+            sendSwipe(_dir);
+        } else {
+            Debug.Log("Tapped screen");
+        }
     }
 
     private void sendSwipe(SwipeDirection dir) {
