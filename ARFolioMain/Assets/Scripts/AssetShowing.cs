877d9ee8cbb9bb976b3214d31700de7aee4c5407
Hayden D.R <hd.rydell@outlook.com>
2021-02-20 15:56:33 +1100

    Started work on projecting found assets into the world space

---
+++ ARFolioMain/Assets/Scripts/AssetShowing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetShowing : MonoBehaviour {
+    void Awake() {
+        ControllerGeneral.activeFolio += setupAsset;
+        Debug.Log("bro");
+    }
+
+    private void setupAsset(ControllerGeneral.activeFolioData data) {
+        var _activeFolio = data.folioActive;
+        var _assetNames = data.folioAssetNames;
+        var _activeAsset = data.assetActive;
+
+        Debug.Log("Here");
+
+        if(_activeAsset.GetType() == typeof(GameObject)) {
+            Debug.Log("Type of image");
+            setupAsImage(_activeAsset);
+        }
+    }
+
+    private void setupAsImage(GameObject activeAsset) {
+        GameObject _testFrame = (GameObject)Resources.Load("Prefabs/testFrame");
+        Mesh _newMesh = _testFrame.GetComponent<MeshFilter>().sharedMesh;		// assign mesh as the frame's mesh
+        
+        Debug.Log("assigning mesh");
+        gameObject.GetComponent<MeshFilter>().mesh = _newMesh;	                // pass it over to the controller
+    }
+}
