08e7863cf0636bb9979dda6db546fc6c310a42b6
Hayden D.R <hd.rydell@outlook.com>
2021-03-16 10:30:53 +1100

    Adjusted script, removed unnecessary namespaces

--- ARFolioMain/Assets/Scripts/ControllerCanvas.cs
+++ ARFolioMain/Assets/Scripts/ControllerCanvas.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
 
 public class ControllerCanvas : MonoBehaviour {
     public  Vector2         screenRes;
@@ -13,36 +10,32 @@ public class ControllerCanvas : MonoBehaviour {
     private void Awake() {
         screenRes = new Vector2(Screen.width, Screen.height);                       // get screen information
         _debugText = transform.GetChild(0).gameObject;                              // get reference to debug text
-        ControllerSwipe.onSwipe += swipeControllerOnSwipe;
+        ControllerSwipe.onSwipe += OnSwipeController;
     }
 
     // Mouse Click input option if in editor
     # if UNITY_EDITOR
     private void Update() {
-        if(Input.GetMouseButtonDown(0)) {
-            if(_mySelectionView == null && Input.mousePosition.y < screenRes.y / 3) 
-                selectionController(true); 
-            else if(_mySelectionView != null && Input.mousePosition.y > screenRes.y / 3) 
-                selectionController(false);
+        if(Input.GetMouseButtonDown(0)) 
+            HandleMousePosition();
+    }
+   
+    private void HandleMousePosition() {
+        if(_mySelectionView == null && Input.mousePosition.y < screenRes.y / 3) {
+            SelectionController(true); 
+            return;            
         }
+        SelectionController(false);
     }
     # endif
 
-    private void swipeControllerOnSwipe(ControllerSwipe.SwipeData data) {
+    private void OnSwipeController(ControllerSwipe.SwipeData data) {
         switch(data.direction) {
             case ControllerSwipe.SwipeDirection.UP:                                 // if direction is up
-                // start to swipe up within the lower third of the screen 
-                if(data.startPosition.y < screenRes.y / 3) {
-                    selectionController(true);
-                    _debugText.GetComponent<Text>().text += "\nSWIPE UP";
-                }
+                SwipeUp(data.startPosition);
                 break;
             case ControllerSwipe.SwipeDirection.DOWN: 
-                // start to swipe within top 2 thinds
-                if(data.startPosition.y > screenRes.y / 3) {
-                    selectionController(false);
-                    _debugText.GetComponent<Text>().text += "\nDOWN";
-                }
+                SwipeDown(data.startPosition);
                 break;
             case ControllerSwipe.SwipeDirection.LEFT: 
                 break;
@@ -51,17 +44,34 @@ public class ControllerCanvas : MonoBehaviour {
         }
     }
 
-    private static void onComplete(object toDestroy) {
+    private void SwipeUp(Vector2 startPos) {
+        if(startPos.y < screenRes.y / 3) {                                          // start to swipe up within the lower third of the screen 
+            SelectionController(true);
+            _debugText.GetComponent<Text>().text += "\nSWIPE UP";
+        }
+    }
+
+    private void SwipeDown(Vector2 startPos) {
+        if(startPos.y > screenRes.y / 3) {                                          // start to swipe within top 2 thirds
+            SelectionController(false);
+            _debugText.GetComponent<Text>().text += "\nDOWN";
+        }
+    }
+
+    private static void OnAnimationComplete(object toDestroy) {
         Destroy((GameObject)toDestroy);
     }
 
-    private void selectionController(bool open) { 
+    private void SelectionController(bool open) { 
         if(open && _mySelectionView == null) {
-            GameObject _viewportSelection = (GameObject)Resources.Load("Prefabs/ViewportSelection");                        // get prefab reference
-            _mySelectionView = Instantiate(_viewportSelection, gameObject.transform, false);                                // create and make its parent the canvas
-            LeanTween.moveY(_mySelectionView, 150f, 0.2f).setEase(easeType);                                               // tween animation
-        } else if(!open && _mySelectionView != null) {                                                                                                            // if already open
-            LeanTween.moveY(_mySelectionView, -200f, 0.1f).setEase(easeType).setOnComplete(onComplete, _mySelectionView);
+            GameObject _viewportSelection = (GameObject)Resources.Load("Prefabs/ViewportSelection");    // load viewport prefab
+            _mySelectionView = Instantiate(_viewportSelection, gameObject.transform, false);            // create and make its parent the canvas
+            LeanTween.moveY(_mySelectionView, 150f, 0.2f).setEase(easeType);                            // tween animation
+            return;
+        } 
+        
+        if(!open && _mySelectionView != null) {                                                         // if already open
+            LeanTween.moveY(_mySelectionView, -200f, 0.1f).setEase(easeType).setOnComplete(OnAnimationComplete, _mySelectionView);
             _mySelectionView = null;
         }
     }
